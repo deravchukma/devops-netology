af951cc1dc309c56b3d271896ddaf005774c4365
Mikhail Deravchuk <deravchukma@gmail.com>
2025-03-18 21:04:33 +0500

    upd 03-ter-homeworks/02

--- homeworks/03-ter-homeworks/02/demo/providers.tf
+++ homeworks/03-ter-homeworks/02/demo/providers.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = "0.124.0"
     }
   }
-  required_version = "~>1.8.4"
+  required_version = ">=1.8.4"
 }
 
 provider "yandex" {
-  #token                    = "do not use!!!"
   cloud_id                 = "b1gn3ndpua1j6jaabf79"
   folder_id                = "b1gfu61oc15cb99nqmfe"
   service_account_key_file = file("~/.authorized_key.json")
