854f8f724ebf6367816b1110088aa7177204ea0e
Mikhail Deravchuk <deravchukma@gmail.com>
2026-02-24 03:15:28 +0500

    upd hw-03

--- homeworks/03-ter-homeworks/03/demo/providers.tf
+++ homeworks/03-ter-homeworks/03/demo/providers.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "yandex" {
   # token                    = "do not use!!!"
-  cloud_id                 = "b1g7s2ff22026b6543bd"
-  folder_id                = "b1gido7jghida49n0l1h"
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
   service_account_key_file = file("~/.authorized_key.json")
   zone                     = "ru-central1-a" #(Optional) 
 }
--- homeworks/03-ter-homeworks/03/demo/variables.tf
+++ homeworks/03-ter-homeworks/03/demo/variables.tf
@@ -5,4 +5,17 @@ variable "public_key" {
   default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIkkg4ZzBkuU7oqK2ujg2yoim2xOOzYkEHDiCQfWPkT udjin@ubuntu"
 }
 
+variable "yc_token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
 
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
