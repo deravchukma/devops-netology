1ad8253dcb426b966cbd4db7dd74003b80a939d0
Mikhail Deravchuk <deravchukma@gmail.com>
2026-02-24 07:33:33 +0500

    upd hw-03

---
+++ homeworks/03-ter-homeworks/03/src/for_each-vm.tf
@@ -0,0 +1,50 @@
+variable "db_each_vm" {
+  type = list(object({
+    vm_name     = string
+    cpu         = number
+    ram         = number
+    disk_volume = number
+    platform_id = string
+  }))
+  default = [
+    {
+      vm_name     = "main"
+      cpu         = 4
+      ram         = 8
+      disk_volume = 50
+      platform_id = "standard-v1"
+    },
+    {
+      vm_name     = "replica"
+      cpu         = 2
+      ram         = 4
+      disk_volume = 25
+    }
+  ]
+}
+
+resource "yandex_compute_instance" "db_vm" {
+  for_each    = { for vm in var.db_each_vm : vm.vm_name => vm }
+  
+  name        = each.value.vm_name
+  platform_id = each.value.platform_id  
+
+  resources {
+    cores  = each.value.cpu
+    memory = each.value.ram
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu-2404-lts.image_id
+	  type     = "network-hdd"
+      size     = each.value.disk_volume
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.example.id]
+  }
+  }
