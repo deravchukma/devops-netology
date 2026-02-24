30acb726f3f5acd6923c3e020d89a559ad61dbcb
Mikhail Deravchuk <deravchukma@gmail.com>
2026-02-24 07:11:55 +0500

    upd hw-03

---
+++ homeworks/03-ter-homeworks/03/src/count-vm.tf
@@ -0,0 +1,26 @@
+resource "yandex_compute_instance" "web" {
+  count = 2
+  name     = "web-${count.index + 1}"
+  hostname = "web-${count.index + 1}"
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 1
+	core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu-2404-lts.image_id
+	  type     = "network-hdd"
+      size     = 15
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.example.id]
+  }
+}
